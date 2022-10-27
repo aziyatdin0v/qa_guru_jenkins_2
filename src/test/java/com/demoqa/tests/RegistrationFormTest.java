79ebc769e843278c840219969e62f91dff5faeee
aziyatdin0v <azat.ziyatdinov@yandex.ru>
2022-10-27 19:53:36 +0300

    moved files from other project

---
+++ src/test/java/com/demoqa/tests/RegistrationFormTest.java
@@ -0,0 +1,48 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.RegistrationFormPage;
+import org.junit.jupiter.api.Test;
+import static com.demoqa.utils.TestData.*;
+import static io.qameta.allure.Allure.step;
+
+public class RegistrationFormTest extends BaseTest {
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    RegistrationFormPage checkTable = new RegistrationFormPage();
+
+    @Test
+    void fillPracticeFormTest() {
+
+        step("Open registrations form", () -> {
+            registrationFormPage.openPage();
+        });
+
+        step("Fill form", () -> {
+            registrationFormPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setGender(gender)
+                    .setPhoneNumber(phoneNumber)
+                    .setBirthDate(day, month, year)
+                    .setSubjects(subject)
+                    .setHobbies(hobbies)
+                    .setUploadPicture(picture)
+                    .setCurrentAddress(address)
+                    .setStateAndCity(state, city)
+                    .setSubmit();
+        });
+
+        step("Check form results", () -> {
+            checkTable.checkResultsTableVisible()
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", phoneNumber)
+                    .checkResult("Date of Birth", day + " " + month + "," + year)
+                    .checkResult("Subjects", subject)
+                    .checkResult("Hobbies", hobbies)
+                    .checkResult("Picture", picture)
+                    .checkResult("Address", address)
+                    .checkResult("State and City", state + " " + city);
+        });
+    }
+}
